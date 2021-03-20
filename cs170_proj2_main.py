70d1d27159cb8318799565a6435c2712e637b5d2
LC <luccas.chang@gmail.com>
2021-03-19 21:43:29 -0700

    might be onto something; for some reason, a variable in search fxn, particularly curr_set_of_feat gets modified after being passed in as an argument to another function; work arounds have been made and hopefully allows the project to work as intended

--- cs170_proj2_main.py
+++ cs170_proj2_main.py
@@ -2,6 +2,7 @@ import pandas
 import numpy
 import random
 import math
+import time
 from itertools import islice
 
 #check if dependencies were installed
@@ -22,60 +23,81 @@ from itertools import islice
 #functions to get aquainted with pandas dataframe traversal
 #print(small_df.tail(5))
 #print(small_df.iloc[200][5])
-def cross_valid_demo(data, curr_set, feature_to_add):
+def cross_valid_demo(data,  features, feature_to_add):
     return random.random()
 
 def feature_search_demo(data):
-    curr_set_of_feat = []
+    best_set_at_level = []
     list_of_acc = []
     #i for index val, x for the actual row
     data_feat = data.columns[1:].values
-    for i in data_feat:
+
+    for i in data_feat: #iterate through the levels
         #col for actual col val
         #print(type(row))
         #[1:] to ignore class
-        #print('on the ' + str(i) + 'th level of the search tree')
-        feature_to_add_at_lvl = []
+        print('on the ' + str(i) + 'th level of the search tree')
+        curr_set_of_feat = []
+        feature_to_add_at_lvl = 0
         best_curr_acc = 0.0
 
-        
-        for j in data_feat:
+        if len(best_set_at_level) != 0:
+            #print(best_set_at_level[-1])
+            curr_set_of_feat.extend(best_set_at_level[-1])
+    
+        for j in data_feat: #iterate through possible additions to previous level's best set
             #print(col)
             #print(type(j))
             #print('j=' + str(j))
             #k = j
-            if int(j) not in curr_set_of_feat:
+            if numpy.int64(j) not in curr_set_of_feat:
+                #print(str(feature_to_add_at_lvl) + ' was not found in ' + str(curr_set_of_feat))
                 print('considering adding feature: ' + str(j))# + ' = ' + str(col))
                 #for some reason ++j doesnt work
                 #k = j + 1
+                #print(curr_set_of_feat)
                 #print(j)
-                accuracy = cross_valid_demo(small_df, curr_set_of_feat, j)
+                accuracy = cross_valid(data, curr_set_of_feat, j)
+                #print(curr_set_of_feat)
+                #print('using feature(s) ' + str(feature_to_add_at_lvl) + ', accuracy is ' + str(accuracy))
                 #print('ditto ' + str(j))
                 if (accuracy - best_curr_acc) > 0.01: #new accuracy is greater than curr
+                    #print(accuracy - best_curr_acc)
                     best_curr_acc = accuracy
+                    #print(curr_set_of_feat)
                     #print(type(best_curr_acc))
                     #print(type(accuracy))
                     #print(j)
-                    feature_to_add_at_lvl.append(j)
+                    feature_to_add_at_lvl=j
                     print('using feature ' + str(feature_to_add_at_lvl) + ' our new best accuracy is ' + str(best_curr_acc))
+                    #print(curr_set_of_feat)
                     #print(type(k))
                     #print(type(feature_to_add_at_lvl))
+                
+                #print(curr_set_of_feat)
+        
+        #curr_set_of_feat.insert(i,feature_to_add_at_lvl)
+        #if len(feature_to_add_at_lvl) != 0:
+        #print('append time')
+        curr_set_of_feat.append(feature_to_add_at_lvl)
+        best_set_at_level.append(curr_set_of_feat)
+        list_of_acc.append(best_curr_acc)
+        print('on level ' + str(i) + ', feature(s) ' + str(curr_set_of_feat) + ' was added to current level with accuracy of ' + str(best_curr_acc))
             #print(j)
             #else:
                 #print('ditto')
             #j+=1 #incr index for print
         #if(feature_to_add_at_lvl):
-        curr_set_of_feat.append(feature_to_add_at_lvl)
-        list_of_acc.append(best_curr_acc)
         #print(type(curr_set_of_feat[0]))
-        #print(curr_set_of_feat)
-        #print('on level ' + str(i) + ', feature ' + str(feature_to_add_at_lvl) + ' was added to current set')
+        #print(type(curr_set_of_feat))
+        
 #            print(f"Index: {i}")
 #        print(f"contents:\n{row}\n")
 #    for row in small_df.itertuples():
 #        print(row.Index + row._1)
-    #print(curr_set_of_feat)
-    print('feature set with largest accuracy is: ' + str(max(list_of_acc)) + ' using feature set ' + str(curr_set_of_feat[list_of_acc.index(max(list_of_acc))]))
+    print(best_set_at_level)
+    print(list_of_acc)
+    print('feature set with largest accuracy is: ' + str(max(list_of_acc)) + ' using feature set ' + str(best_set_at_level[list_of_acc.index(max(list_of_acc))]) + ' at level ' + str(list_of_acc.index(max(list_of_acc))))
     #print('our best set of features are ' + str(curr_set_of_feat) + ' with an accuracy of ' + str(best_curr_acc))
     #print('finished: ' + str(curr_set_of_feat))
 #, curr_set, feature_to_add)
@@ -84,7 +106,11 @@ def cross_valid(data, curr_set, feature_to_add):
     temp_data = data.copy()
 
     #create a temp list of features to focus on, including the feature at hand
-    temp_set = curr_set
+    #print(curr_set)
+    #print(feature_to_add)
+    temp_set = []
+    if len(curr_set) != 0:
+        temp_set.extend(curr_set)
     temp_set.append(feature_to_add)
     #print('temp set ' + str(temp_set))
 #    for x in range(1,11):
@@ -97,12 +123,12 @@ def cross_valid(data, curr_set, feature_to_add):
     for i, row in temp_data.iterrows():
         obj_to_classify = row[1:]
         #print(obj_to_classify)
+        label_obj_to_class = row[0]
+        #print('label='+str(label_obj_to_class))
 
         nearest_neighbor_dist = 1000000000.0
         nearest_neighbor_loc = 1000000000.0
-
-        label_obj_to_class = row[0]
-        #print('label='+str(label_obj_to_class))
+        nearest_neighbor_label = 0.0
 
         for k, subrow in temp_data.iterrows():
             #print('subraow='+str(subrow))
@@ -112,8 +138,8 @@ def cross_valid(data, curr_set, feature_to_add):
                 #print(subrow)
                 distance = math.sqrt(sum((obj_to_classify - subrow[1:]))**2.0)
                 #print(distance)
-
-                if (distance - nearest_neighbor_dist) < 0.001:
+                #dist_diff = 
+                if (distance - nearest_neighbor_dist) < 0.0001:
                     nearest_neighbor_dist = distance
                     nearest_neighbor_loc = k
                     #print(data[nearest_neighbor_loc][0])
@@ -124,7 +150,7 @@ def cross_valid(data, curr_set, feature_to_add):
 
         if label_obj_to_class == nearest_neighbor_label:
             num_correct_class+=1
-        
+    #print('numcorrect = ' + str(num_correct_class))
     accuracy = num_correct_class/len(data.index)
     print('with features ' + str(temp_set) + ', we have an accuracy of ' + str(accuracy))
     return accuracy
@@ -132,13 +158,18 @@ def cross_valid(data, curr_set, feature_to_add):
 
 
 #the data files are split "__", two blank spaces
-small_df = pandas.read_csv('CS170_SMALLtestdata__70.txt', sep = '  ', header = None)
+start_time = time.time()
+small_df = pandas.read_csv('CS170_SMALLtestdata__70_smaller.txt', sep = '  ', header = None)
 feature_search_demo(small_df)
-#cross_valid(small_df, [1,9], 5)
+print("time = " + str(time.time() - start_time) + " seconds")
+#cross_valid(small_df, [1,9], 2)
 
 #temp_arr = [[3,4,5], [1,2]]
+#targ = [1]
+#if targ not in temp_arr:
+#    print('yeet')
 #temp_arr2 = [7,8,10]
-#temp_arr.append(temp_arr2)
+#temp_arr.append(5)
 #print(temp_arr)
 #temp = pandas.DataFrame(
 #    [[1,2,3],
